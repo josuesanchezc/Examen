4d806ab323bde7ef8012a707f8b590fe2736193e
josuesanchezc <josuesanchezc@live.com.mx>
2014-10-30 23:13:01 -0600

    actiualizacion 2

--- clases/Preguntas.php
+++ clases/Preguntas.php
@@ -53,45 +53,5 @@ class Preguntas {
         echo '</form>'."\n";
 
     }
-/*public function calificar($preg){
-
-    if(!$preg||!$respDada){
-        echo "<p>Acceso invalido</p>";
-    }
-    else{
-        $conexion = mysql_connect("localhost","root","qwerty");
-        if(!$conexion){
-            echo "<p>Error: No se puede conectar al servidor<br>\n";
-            echo "<a href=\"index.php\"> Regresar al homie ese</a> </p>\n";
-        }
-        else{
-            $bd = mysql_select_db("examen",$conexion);
-            if(!$bd){
-                echo "<p>Error: No se pudo seleccionar la bd<br>\n";
-                echo "<a href=\"Index.php\"> Regresar al homie ese</a> </p>\n";
-            }
-            else{
-                $calif=0;
-                $consulta = mysql_query("select nombre from usuarios where matricula='$matricula'",$conexion);
-                echo "<h2> Calificacion </h2>\n";
-                echo "<p> Alumno</p>\n";
-                for($cju=0;$cju<sizeof($preg);$cju++){
-                    $consulta = mysql_query("select idr from corresponde where idp='$preg[$cju] and sipi=1'",$conexion);
-                    $idres = mysql_result($consulta,0,'idr');
-                    $consulta = mysql_query("select respuesta from respuestas where idr='$idres'",$conexion);
-                    $respuestidirijilla = mysql_result($consulta,0,'respuesta');
-                    if($respDada[$cju]==$respuestidirijilla) $calif+=1;
-                }
-                mysql_query("update usuarios set calif='$calif', presento=1 where matricula='$matricula'",$conexion);
-                mysql_Close($conexion);
-                echo "<p>Ha obtenido una calificacion ".($calif>=7?"aprobatoria":"reprobatoria")." de $calif</p>";
-                echo "<p><a href=\"Index.php\"> Regresar al homie ese</a> </p>\n";
-            }
-        }
-    }
-
-
-}*/
-
 }
 ?>
